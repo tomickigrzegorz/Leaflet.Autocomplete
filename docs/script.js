a82cf64661cc8efcb4782dd526d4bfbab599bcb7
Grzegorz Tomicki <gtomik@gmail.com>
2020-08-20 15:52:33 +0200

    update lib

--- docs/script.js
+++ docs/script.js
@@ -56,7 +56,7 @@ window.addEventListener('DOMContentLoaded', function () {
           return `
           <li class="autocomplete-item loupe" data-elements='${JSON.stringify(jsonData).replace(/[\/\(\)\']/g, "&apos;")}' role="option" aria-selected="false">
             <p>
-              ${properties.display_name}
+              ${properties.display_name.replace(regex, (str) => `<b>${str}</b>`)}}
             </p>
         </li > `;
         }
